e081ca1d176e4fedcdb0cd4321d06e2ccf75ad7c
JINZEJINZE <79951112+JINZEJINZE@users.noreply.github.com>
2022-01-17 18:28:42 -0500

    Add files via upload

---
+++ Text Based Advernture Game/src/Main/Dungeon.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Dungeon {
+    private String dungeonName;
+    private int dungeonLevel;
+    private String[] dungeonMobName;
+    private String dungeonPrefix;
+
+    public Dungeon(int DungeonLevel) {
+
+    }
+
+}
